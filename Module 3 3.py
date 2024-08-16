8c65c91177cf71946c96b5cfe200406010bfa37b
Pavel-den <p-w.den@mail.ru>
2024-08-16 12:51:46 +0300

    Add files via upload

---
+++ Module 3 3.py
@@ -0,0 +1,14 @@
+def print_params(a = 1, b = 'строка', c = True):
+    print(a, b, c)
+print_params()
+print()
+print_params(b = 25)
+print_params(c = [1, 2, 3])
+print()
+values_list = [False, 5, 'Text']
+values_dict = {'a': True, 'b': 'Dog', 'c': 89}
+print_params(*values_list)
+print_params(**values_dict)
+print()
+values_list_2 = [54.32, 'Строка']
+print_params(*values_list_2, 42)
